85e6d4553a2867f7d44e31f32a974fb6938e3d97
ianiv <ihojmanb@gmail.com>
2025-09-20 22:06:12 +0300

    expenses form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
@@ -29,22 +29,17 @@ function HomePage() {
   )
 }
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={HomePage}/>
-      {/* Fallback to 404 */}
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
